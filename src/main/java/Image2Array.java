c23c51b384db93b0205a4018083be377e51066dd
braydenleung-Git <brayden.leung@gmail.com>
2024-05-31 23:24:57 -0400

    psd to array prototype complete
    Signed-off-by: braydenleung-Git <brayden.leung@gmail.com>

--- src/main/java/Image2Array.java
+++ src/main/java/Image2Array.java
@@ -1,7 +1,7 @@
-
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 import org.apache.commons.imaging.Imaging;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * This class is used to interpret a specified image to an array
@@ -10,9 +10,10 @@ public class Image2Array {
 
     private static int[][] pixelArray;
     private static int[][] transcodedArray;
-    public static void processImage(String imgPath){
+
+    public static void processImage(String imgPath) {
         try {
-            BufferedImage img = Imaging.getBufferedImageFromPng(imgPath);
+            BufferedImage img = Imaging.getBufferedImage(new File(imgPath));
             int width = img.getWidth();
             int height = img.getHeight();
             pixelArray = new int[height][width];
@@ -24,76 +25,32 @@ public class Image2Array {
                     pixelArray[y][x] = rgb;
                 }
             }
-            transcodeArray(height,width);
+            transcodeArray(height, width);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
     }
 
-    private static void transcodeArray(int x, int y){
+    private static void transcodeArray(int y, int x) {
         int current = 0;
-        for (int i = 0; i <x; i++) {
-            for (int j = 0; j < y; j++) {
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
                 current = pixelArray[i][j];
-                if(current == 0xFF000000){
+                if (current == 0xFF000000) {
                     transcodedArray[i][j] = 1;
                 } else if (current == 0xFFFFFFFF) {
                     transcodedArray[i][j] = 0;
-                }
-                else 
-            }
-        }
-    }
-}
-
-
-/*
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-
-public class ImageToArray {
-    public static void main(String[] args) {
-        try {
-            // Load the image
-            BufferedImage image = ImageIO.read(new File("path_to_your_image.png"));
-
-            // Get image dimensions
-            int width = image.getWidth();
-            int height = image.getHeight();
-
-            // Initialize the array
-            int[][] pixelArray = new int[height][width];
-
-            // Extract pixel data
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    // Get RGB value of the pixel
-                    int rgb = image.getRGB(x, y);
-                    pixelArray[y][x] = rgb;
+                } else {
+                    System.out.println("Wrong pixel value: " + current);
+                    break;
                 }
             }
-
-            // Print pixel array as text representation
-            printPixelArray(pixelArray);
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
-    public static void printPixelArray(int[][] pixelArray) {
-        for (int y = 0; y < pixelArray.length; y++) {
-            for (int x = 0; x < pixelArray[y].length; x++) {
-                // Convert the RGB value to a character representation (simplified)
-                char c = (char)((pixelArray[y][x] & 0xFF) % 128);
-                System.out.print(c + " ");
-            }
-            System.out.println();
-        }
+    public static int[][] getTranscodedArray() {
+        return transcodedArray;
     }
+
 }
-
- */
---
+++ src/main/java/i2atest.java
@@ -0,0 +1,12 @@
+public class i2atest {
+    public static void main(String[] args) {
+        Image2Array.processImage("src/main/resources/test 1.psd");
+        int[][] array = Image2Array.getTranscodedArray();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
