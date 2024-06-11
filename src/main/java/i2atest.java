26b6176ea11199a3331f7795b6a899fe6a7771f5
braydenleung-Git <brayden.leung@gmail.com>
2024-06-11 15:07:58 -0400

    test file now allows user input based test case
    Signed-off-by: braydenleung-Git <brayden.leung@gmail.com>

--- src/main/java/i2atest.java
+++ src/main/java/i2atest.java
@@ -3,22 +3,13 @@ import java.util.Scanner;
 
 public class i2atest {
     public static void main(String[] args) throws IOException {
-        /*.writeToJson();
+        testCases.writeToJson();
         System.out.println("Which file would you like to see?");
         for (int i = 0; i < testCases.getItemList().size(); i++) {
             System.out.println(testCases.getItemList().get(i));
         }
         int input = Integer.parseInt(new Scanner(System.in).nextLine());
-        System.out.println(testCases.getItem(input));*/
-        Image2Array.processImage("./TestCases/Square.png");
-
+        Image2Array.processImage(testCases.getItem(input).toString());
         shapelogic.shapel(Image2Array.getTranscodedArray());
     }
 }
-/*
-Image2Array.processImage("./Rectangle.png");
-transcoded array
-find pixel
-return shape
-test case
- */
