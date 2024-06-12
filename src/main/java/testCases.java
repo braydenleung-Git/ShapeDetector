89f30f52b1e19dd172dafe2065727695a49d1513
braydenleung-Git <brayden.leung@gmail.com>
2024-06-11 23:21:53 -0400

    added new library, json formatting library, fixed for loop does not loop in line 47, moving on to gui
    Signed-off-by: braydenleung-Git <brayden.leung@gmail.com>

--- src/main/java/testCases.java
+++ src/main/java/testCases.java
@@ -2,6 +2,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.module.kotlin.KotlinModule;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,53 +13,55 @@ import java.util.List;
 public class testCases {
     //set methods back to private later, there should have been something else calling it
     private final static String jsonFile = "./src/main/resources/testSolutions.json";
+
     public static void writeToJson(){
-        final String directoryPath = "./TestCases";
+        final String directoryPath = "./src/main/resources/TestCases";
         File directory = new File(directoryPath);
         List<String[]> newlyAddedFiles = new ArrayList<>();
         List<String[]> probe = new ArrayList<>();
-
+        boolean probe_bool = false;
         File[] files = directory.listFiles();
         //for each file within the file array
-        if(files != null){
-            for (File file : files) {
-                //if it is a file, has an extension of .png
-                if (file.isFile()&&file.getName().substring(file.getName().length()-3).equals(".png")) {
+        probe_bool = files != null;
+        if(probe_bool){
+            for(File file : files){
+                //if the file has an extension of .png
+                if (file.getName().endsWith(".png")) {
+
                     //copy the relative path & file name(including extensions)
                     String relativePath = file.getAbsolutePath().substring(directoryPath.length() + 1);
                     String fileName = file.getName();
                     //add this object to the filepath and names
                     newlyAddedFiles.add(new String[]{relativePath, fileName});
-                    probe = newlyAddedFiles;
                 }
             }
         }
         try{
-            ObjectMapper objectMapper = new ObjectMapper();
+            ObjectMapper objectMapper = new ObjectMapper()
+                    .registerModule(new KotlinModule())
+                    .enable(SerializationFeature.INDENT_OUTPUT);
             JsonNode rootNode = objectMapper.readTree(new FileInputStream(jsonFile));
             ArrayNode rootArrayNode = (ArrayNode) rootNode;
             boolean found = false;
             for (String[] filePathAndName : newlyAddedFiles) {
-                JsonNode fileNode = rootNode.get("FileName");
-                for(JsonNode node : fileNode){
+                for(JsonNode node : rootNode){
                     //if the current filename exist within the json mark it as "found"
-                    if (node.asText().equals(filePathAndName[1])){
-                        fileNode = rootNode.get(filePathAndName[0]);
+                    probe_bool = node.get("FileName").asText().equals(filePathAndName[1]);
+                    if (probe_bool){
                         found=true;
                         break;
                     }else{
                         found=false;
                     }
-                    break;
                 }
                 //if the file is not "found", then add the file to the json file
                 if (!found){
                     ObjectNode addNode = objectMapper.createObjectNode();
                     addNode.put("Name",filePathAndName[1].substring(0,filePathAndName[1].length()-4));
                     addNode.put("FileName",filePathAndName[1]);
-                    addNode.put("Path",filePathAndName[0]);
+                    addNode.put("Path",filePathAndName[0].substring(directoryPath.length()-3));
                     rootArrayNode.add(addNode);
-                    objectMapper.writeValue(new File(jsonFile), fileNode);
+                    objectMapper.writeValue(new File(jsonFile), rootNode);
 
                 }
             }
