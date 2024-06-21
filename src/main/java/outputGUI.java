6009177544228191fb0ba3335300f4b5a07d0445
Tidal_Maze <tidalmaze0432@gmail.com>
2024-06-21 13:30:04 -0400

    final gui edit

--- src/main/java/outputGUI.java
+++ src/main/java/outputGUI.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
+
 public class outputGUI extends JPanel {
     public static int circles = 0;
     public static int rectangles = 0;
@@ -15,12 +16,15 @@ public class outputGUI extends JPanel {
         setBackground(Color.BLACK);
         setLayout(null);
 
+
         // Display the path of the dropped file
         JLabel label = new JLabel("Dropped file: " + filePath);
         label.setBounds(20, 20, 360, 20);  // Position the label
         label.setFont(main.HelvetciaNeue_Cond_B_05.deriveFont(14f));
         label.setForeground(Color.WHITE);
         add(label);
+
+
 
 
         // Add test statements
@@ -30,22 +34,30 @@ public class outputGUI extends JPanel {
         circle.setForeground(Color.WHITE);
 
 
-        JLabel rect = new JLabel("The amount of rectangles are: " + circles);
+
+
+        JLabel rect = new JLabel("The amount of rectangles are: " + rectangles);
         rect.setBounds(20, 190, 200, 40);
         rect.setFont(main.HelvetciaNeue_Cond_B_05.deriveFont(14f));
         rect.setForeground(Color.WHITE);
 
 
-        JLabel square = new JLabel("The amount of squares are: " + circles);
+
+
+        JLabel square = new JLabel("The amount of squares are: " + squares);
         square.setBounds(20, 210, 200, 40);
         square.setFont(main.HelvetciaNeue_Cond_B_05.deriveFont(14f));
         square.setForeground(Color.WHITE);
 
 
-        JLabel tri = new JLabel("The amount of triangles are: " + circles);
+
+
+        JLabel tri = new JLabel("The amount of triangles are: " + triangles);
         tri.setBounds(20, 230, 200, 40);
         tri.setFont(main.HelvetciaNeue_Cond_B_05.deriveFont(14f));
         tri.setForeground(Color.WHITE);
+
+
 
 
         JButton repeat = new JButton("Return to Start");
@@ -53,6 +65,7 @@ public class outputGUI extends JPanel {
             main.changeLayout(400, 400, "Start Menu");
         });
         repeat.setBounds(20, 280, 120, 30);
+
 
         // Optionally load and display the image if it's an image file
         if (filePath != null && (filePath.endsWith(".png") )) {
@@ -79,6 +92,7 @@ public class outputGUI extends JPanel {
             }
         }
 
+
         // Adding labels and button
         add(circle);
         add(rect);
@@ -86,7 +100,9 @@ public class outputGUI extends JPanel {
         add(tri);
         add(repeat);
 
+
     }
+
 
     public static void reset(){
         circles = 0;
@@ -95,3 +111,4 @@ public class outputGUI extends JPanel {
         squares = 0;
     }
 }
+
